11d1e861863adbd91488a0d34d4f78e0995f8a80
ferhatkaplan <ferhatkaplan2742@gmail.com>
2022-06-23 22:26:31 +0300

    Initial commit

---
+++ src/main/java/com/ferhatkaplan/KullaniciKaydiApplication.java
@@ -0,0 +1,13 @@
+package com.ferhatkaplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KullaniciKaydiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KullaniciKaydiApplication.class, args);
+    }
+
+}
